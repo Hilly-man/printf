89f2f9f57c194a65a379f13707354dc6f09c6638
Hilly-man <hilkiahe@gmail.com>
2023-05-04 07:14:02 -0700

    Quick corrections on printf

--- c_s_%_handler.c
+++ c_s_%_handler.c
@@ -1,24 +1,19 @@
-#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
+
 /**
  * _printf - produces output according to a format
  * @format: format string
- *
- * This function produces output according to a format string. The format string
+ * This function produces output according to a format string
  * is composed of zero or more directives, which begin with a '%' character.
  * The following conversion specifiers are supported:
- *
  *   - %c: prints a single character
  *   - %s: prints a null-terminated string
  *   - %%: prints a single '%' character
- *
- * Returns: the number of characters printed (excluding the null byte used to
+ * Return: the number of characters printed (excluding the null byte used to
  * end output to strings)
- * l
- * l
  */
 int _printf(const char *format, ...)
 {
@@ -26,7 +21,6 @@ int _printf(const char *format, ...)
 	int len = 0;
 
 	va_start(args, format);
-
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -34,34 +28,33 @@ int _printf(const char *format, ...)
 			format++;
 			switch (*format)
 			{
-				case 'c': {
-						  char c = va_arg(args, int);
+				case 'c':{
+						char c = va_arg(args, int);
 
-						  len += write(1, &c, 1);
-						  break;
-					  }
-				case 's': {
+						len += write(1, &c, 1);
+						break;
+					}
+				case 's':{
 						char *str = va_arg(args, char *);
 
 						len += write(1, str, strlen(str));
 						break;
-					  }
-				case '%': {
+					}
+				case '%':{
 						len += write(1, "%", 1);
 						break;
-					  }
-				default: {
+					}
+				default:{
 						break;
-					 }
+					}
 			}
 		}
 		else
-		{
+			{
 			len += write(1, format, 1);
 		}
 		format++;
 	}
-
 	va_end(args);
 	return (len);
 }
